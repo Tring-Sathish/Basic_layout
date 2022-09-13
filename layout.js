d49288a4cd6cbde2c9b8487377543387f32ab72c
Tringapps-Sathish <102800173+Tringapps-Sathish@users.noreply.github.com>
2022-09-13 10:25:40 +0530

    Update layout.js

--- layout.js
+++ layout.js
@@ -1,24 +1,3 @@
-/*var acc = document.getElementsByClassName("tab_button");
-var i, j;
-
-for (i = 0; i < acc.length; i++) {
-  console.log(acc[i]);
-  acc[i].addEventListener("click", function () {
-    removeToggle();
-    this.classList.toggle("tab_button--active");
-  });
-}
-function removeToggle() {
-  var ele = document.getElementsByClassName("tab_button");
-  var j;
-  for (j = 0; j < ele.length; j++) {
-    console.log(ele);
-    if (ele[j].classList.contains("tab_button--active")) {
-      ele[j].classList.remove("tab_button--active");
-    }
-  }
-}*/
-
 var acc = document.getElementsByClassName("tab_button");
       for (let i = 0; i < acc.length; i++) {
         acc[i].addEventListener("click", function () {
